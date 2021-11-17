42e258115aef04eb9b43a06343a202095d891505
ChaeHyeonyeong <gusdudco6@naver.com>
2021-11-17 17:39:46 +0900

    댓글을 포스트 상세 페이지로 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 # Create your tests here.
@@ -42,6 +42,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_python)
         self.post_003.tags.add(self.tag_python_kor)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_trump,
+            content='첫번째 댓글입니다.'
+        )
 
     def navbar_test(self, soup):
         # 네비게이션바가 있다
@@ -165,14 +171,13 @@ class TestView(TestCase):
         self.assertTrue(tag_str_input)
         self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
 
-
         # 실제 수정후 확인
-        response=self.client.post(update_url,
-                                  {'title': '세번째 포스트 수정',
-                                   'content': '안녕? 우리는 하나/... 반가와요',
-                                   'category': self.category_culture.pk,
-                                   'tags_str': '파이썬 공부; 한글 태그; some tag'
-                                   }, follow=True)
+        response = self.client.post(update_url,
+                                    {'title': '세번째 포스트 수정',
+                                     'content': '안녕? 우리는 하나/... 반가와요',
+                                     'category': self.category_culture.pk,
+                                     'tags_str': '파이썬 공부; 한글 태그; some tag'
+                                     }, follow=True)
 
         soup = BeautifulSoup(response.content, 'html.parser')
         main_area = soup.find('div', id='main-area')
@@ -265,3 +270,8 @@ class TestView(TestCase):
         self.assertIn(self.post_001.content, post_area.text)
 
         self.assertIn(self.user_james.username.upper(), post_area.text)
+
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
