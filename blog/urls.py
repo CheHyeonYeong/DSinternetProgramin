10aa5d6cc5710eacfa6e25f7f28823628dbf4bb6
ChaeHyeonyeong <gusdudco6@naver.com>
2021-11-24 16:26:24 +0900

    검색기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [  # 서버 ip/blog/
     #    path('<int:pk>/', views.single_post_page),
     #    path('', views.index),  # 서버 ip/blog
+    path('search/<str:q>/',views.PostSearch.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
